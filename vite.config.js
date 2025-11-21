8601b68abf4db90ca53ffca5843912a05786158f
Tugs-ochir <B.Tugsochir1@gmail.com>
2025-11-21 21:39:19 +0800

    Add allowed hosts for Render deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  preview: {
+    allowedHosts: ['tugs-ochir.onrender.com', 'tugs-ochir.info']
+  },
   server: {
     port: 3000,
     open: true
