63f23eefa49157bd4dabd544f91fde470af19014
Tugs-ochir <B.Tugsochir1@gmail.com>
2025-11-21 21:29:10 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,14 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser'
   }
 })
